79c96989b5cb454fc6419e3b5a6af1be1d678341
Nakul Chouksey <71.nakul@gmail.com>
2026-06-20 01:14:08 +0530

    chore: add ring, jetty, reitit dependencies and Clojure learning examples

---
+++ clojure-learning/code1.clj
@@ -0,0 +1,10 @@
+(println "Hello World")
+
+(def name "Nakul")
+
+(println name)
+
+(defn square [x]
+  (* x x))
+
+(println (square 5))
---
+++ clojure-learning/main.clj
@@ -0,0 +1,32 @@
+;; ----------------- print 
+;; (println "Hello Nakul")
+
+
+;; ----------------- variables 
+;; (def name "Nakul")
+;; (println name)
+
+
+;; ----------------- operations 
+;; (println (+ 1 2))
+
+
+;; ----------------- functions
+(defn greet [name]
+  (println "Hello" name))
+
+(greet "Nakul")
+
+
+;; ----------------- Collections
+;; (def nums [1 2 3 4])
+
+
+;; ----------------- Objects
+;; (def user {:name "Nakul"
+;;            :age 21})
+
+
+;; ----------------- sets
+;; #{1 2 3}
+
